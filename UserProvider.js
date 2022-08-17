dbebe2b73ab4d5befb154f06af1abf5fe16ee84b
Hemant <hrsmart2150@gmail.com>
2022-08-17 23:21:00 +0530

    Added update profile functionality

--- UserProvider.js
+++ UserProvider.js
@@ -1,12 +1,36 @@
 import React, { useContext } from 'react';
+import { useUserData, useUserId } from '@nhost/nextjs'
+import { gql, useQuery } from '@apollo/client'
 
 const UserContext = React.createContext(null);
 
+const GET_USER_QUERY = gql`
+  query GetUser($id: uuid!) {
+    user(id: $id) {
+      id
+      email
+      displayName
+      metadata
+      avatarUrl
+    }
+  }
+`
+
 export function UserProvider({ children = null }) {
-  const user = null;
-  return (
-    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
-  );
+  const id = useUserId()
+  const { loading, error, data } = useQuery(GET_USER_QUERY, {
+    variables: { id },
+    skip: !id
+  })
+  const user = data?.user
+
+  if (error) {
+    return <p>Something went wrong. Try to refresh the page.</p>
+  }
+  if (loading) {
+    return null
+  }
+  return <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
 }
 
 export function useUserContext() {
